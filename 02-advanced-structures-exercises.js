c58aab4ecd5d245d7886f072c870039eddcac8a5
HectorIglesias <Hector.Iglesias.1A@gmail.com>
2025-06-02 16:54:39 +0200

    Ejercicios 2_!
    Ejercicios del 4 al 7 de la segunda parte del curso

--- 02-advanced-structures-exercises.js
+++ 02-advanced-structures-exercises.js
@@ -28,25 +28,64 @@ console.log(flat_array2)
 let flat_array3 = array.flat(3)
 console.log(flat_array3)
 
-
-
 console.log("\n\nEjercicio 4")
 // 4. Ordena un array de números de mayor a menor
+let array_sort = numeros.sort((a,b) => b - a)
+console.log(array_sort)
 
 console.log("\n\nEjercicio 5")
 // 5. Dados dos sets, encuentra la unión, intersección y diferencia de ellos
+let setA = new Set([2, 3, 5, 7])
+let setB = new Set([1, 3, 5, 7, 8])
+
+let union = new Set([...setA, ...setB])
+console.log(union)
+let intersection = new Set([...setA].filter(element => setB.has(element)))
+console.log(intersection)
+let difference = new Set([...setA].filter(element => !setB.has(element)))
+console.log(difference)
 
 console.log("\n\nEjercicio 6")
 // 6. Itera los resultados del ejercicio anterior
+union.forEach(element => console.log(element))
+
+intersection.forEach(element => console.log(element))
+
+difference.forEach(element => console.log(element))
 
 console.log("\n\nEjercicio 7")
 // 7. Crea un mapa que almacene información se usuarios (nombre, edad y email) e itera los datos
+let my_map = new Map([
+    ["nombre", "Héctor"],
+    ["edad", 30],
+    ["email", "correo"],
+], [
+    ["nombre", "Iván"],
+    ["edad", 28],
+    ["email", "correo1"],
+], [
+    ["nombre", "Ana"],
+    ["edad", 17],
+    ["email", "correo3"],
+]
+)
+
+my_map.forEach((value, key) => console.log(`${key}: ${value}`))
 
 console.log("\n\nEjercicio 8")
 // 8. Dado el mapa anterior, crea un array con los nombres
+let array_nombres = []
+my_map.forEach((value, key) => array_nombres.push(value))
+
+console.log(array_nombres)
 
 console.log("\n\nEjercicio 9")
 // 9. Dado el mapa anterior, obtén un array con los email de los usuarios mayores de edad y transfórmalo a un set
+let array_correos = []
+const map_transformado = Array.from(my_map)
+//my_map.forEach()
+
+console.log(array_correos)
 
 console.log("\n\nEjercicio 10")
 // 10. Transforma el mapa en un objeto, a continuación, transforma el objeto en un mapa con clave el email de cada usuario y como valor todos los datos del usuario
